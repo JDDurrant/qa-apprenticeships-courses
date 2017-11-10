e9e2b7021bc8b2af58749173daec7ba2f416217e
Jack Durrant <jack@durrant.tech>
2017-11-10 15:39:30 +0000

    minor changes

--- 01-induction-and-java-fundamentals/13_Intro_To_Testing/src/Starter/Volume/Volume.java
+++ 01-induction-and-java-fundamentals/13_Intro_To_Testing/src/Starter/Volume/Volume.java
@@ -6,6 +6,15 @@ public class Volume {
 	private int pints;
 	private boolean validVolume;
 
+	// Constructor to create a volume
+	public Volume(int gallons, int pints) {
+
+		this.validVolume = validate(gallons, pints);
+
+		this.gallons = gallons;
+		this.pints = pints;
+	}
+
 	public int getGallons() {
 		return this.gallons;
 	}
@@ -35,21 +44,26 @@ public class Volume {
 		return "";
 	}
 
-	// Constructor to create a volume
-	public Volume(int gallons, int pints) {
-//		TODO Move constructor methods to the top of the class
-		this.validVolume = validate(gallons, pints);
-
-		this.gallons = gallons;
-		this.pints = pints;
-	}
-
 	// Add parameters representing gallons and pints to the
 	// encapsulated data
-	public void addVolume(int gallons2, int pints2) {
-//		TODO Use better variable names
-		if(this.validVolume) {
+	public void addVolume(int addedGallons, int addedPints) {
 
+		if(this.validVolume) {
+			if(!validate(addedGallons, addedPints)) {
+				this.validVolume = false;
+				return;
+			}
+
+			int totalGallons = this.gallons + addedGallons;
+			int totalPints = this.pints + addedPints;
+
+			while(totalPints >= 8) {
+				totalGallons++;
+				totalPints -= 8;
+			}
+
+			this.gallons = totalGallons;
+			this.pints = totalPints;
 
 			validate(this.gallons, this.pints);
 		}
@@ -57,8 +71,27 @@ public class Volume {
 
 	// Subtract parameters representing gallons and pints from the
 	// encapsulated data
-	public void subtractVolume(int gallons2, int pints2) {
+	public void subtractVolume(int subtractedGallons, int subtractedPints) {
 
+		if(this.validVolume) {
+			if(!validate(subtractedGallons, subtractedPints)) {
+				this.validVolume = false;
+				return;
+			}
+
+			int totalGallons = this.gallons - subtractedGallons;
+			int totalPints = this.pints - subtractedPints;
+
+			while(totalPints < 0) {
+				totalGallons--;
+				totalPints += 8;
+			}
+
+			this.gallons = totalGallons;
+			this.pints = totalPints;
+
+			validate(this.gallons, this.pints);
+		}
 	}
 
 	private int convertToPints(int gallons, int pints) {
--- 01-induction-and-java-fundamentals/14_Working_With_Files/src/Starter/FileUtils.java
+++ 01-induction-and-java-fundamentals/14_Working_With_Files/src/Starter/FileUtils.java
@@ -3,9 +3,8 @@ package Starter;
 import java.io.*;
 
 public class FileUtils {
-	
-	public static String lastLineInFile(String inputFile) 
-                                          throws IOException {
+
+	public static String lastLineInFile(String inputFile) throws IOException {
 		return "";
 	}
 
@@ -13,17 +12,15 @@ public class FileUtils {
 	// input file (inputFile) to this new file, but convert the
 	// content of the file to lower case.
 	// If the original file exists it should be overwritten.
-	public static void convertFileToLowerCase(String inputFile,
-			String outputFile) throws IOException {
+	public static void convertFileToLowerCase(String inputFile, String outputFile) throws IOException {
 
 	}
 
 	// This method should create a new file (outputFile) and copy the
 	// input file (inputFile) to this new file,
 	// but convert the content to upper case.
-	
-	public static void convertFileToUpperCase(String inputFile,
-			String outputFile) throws IOException {
+
+	public static void convertFileToUpperCase(String inputFile, String outputFile) throws IOException {
 
 	}
 
@@ -31,8 +28,7 @@ public class FileUtils {
 	// of lines that start with the specified String (in the parameter match).
 	// The file to be searched is the input file.
 	// The search should be case sensitive.
-	public static int linesThatStartWith(String inputFile, String match)
-			throws IOException {
+	public static int linesThatStartWith(String inputFile, String match) throws IOException {
 
 		return 0;
 	}
@@ -41,18 +37,16 @@ public class FileUtils {
 	// of lines that start with the specified String (in the parameter match).
 	// The file to be searched is the input file.
 	// The search should ignore case.
-	public static int linesThatStartWithCaseInsensitive(String inputFile,
-			String match) throws IOException {
+	public static int linesThatStartWithCaseInsensitive(String inputFile, String match) throws IOException {
 
 		return 0;
 	}
 
 	// This method should return an integer representing the number of lines
 	// that contain the specified String (in the parameter match).
-	// The file to be searched is the input file. 
+	// The file to be searched is the input file.
 	// The search should ignore case.
-	public static int linesThatContainCaseInsensitive(String inputFile,
-			String match) throws IOException {
+	public static int linesThatContainCaseInsensitive(String inputFile, String match) throws IOException {
 
 		return 0;
 	}
